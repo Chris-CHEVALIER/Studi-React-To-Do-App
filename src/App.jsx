c283c8eeb593158dfebe7a88bb91583e3c16c6c8
Chevalier <chevalierchris34@gmail.com>
2021-08-30 10:03:40 +0200

    Handle create list feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import logo from './logo.png';
 import './App.css';
 import MyButton from './components/MyButton';
+import ListModal from './components/ListModal';
 import { PlusOutlined } from '@ant-design/icons';
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  console.log(isModalVisible);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,11 +19,12 @@ export default function App() {
         </p>
         <MyButton
           tooltip="Ajouter une liste"
-          onClick={() => console.log("Le bouton a été cliqué")}
+          onClick={() => setIsModalVisible(true)}
           icon={<PlusOutlined />}
         >
           Ajouter une liste
         </MyButton>
+        <ListModal modalTitle="Ajouter une liste" isVisible={isModalVisible} handleCancel={() => setIsModalVisible(false)} />
       </header>
     </div>
   );
--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { Button, Tooltip } from 'antd';
 
 export default function MyButton(props) {
-    return (
-        <Tooltip title={props.tooltip}>
-          <Button
-            shape="round"
-            type="primary"
-            size="large"
-            onClick={props.onClick}
-            icon={props.icon}
-          >
-            {props.children}
-          </Button>
-        </Tooltip>
-    );
+  return (
+    <Tooltip title={props.tooltip}>
+      <Button
+        shape="round"
+        type="primary"
+        size="large"
+        onClick={props.onClick}
+        icon={props.icon}
+      >
+        {props.children}
+      </Button>
+    </Tooltip>
+  );
 }
